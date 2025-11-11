b22ac005a7cbfc6b26da456767576390c649da8b
Safal <bhattarairobert@gmail.com>
2025-11-11 10:53:06 +0545

    Task 1: Operators Done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Week 03 Assignment");
+﻿using System;
+
+namespace Program
+{
+    class Operators
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Denominator cannot be zero.");
+            }
+            return (double)a / b;
+        }
+
+        public void OddEvenFinder(int number)
+        {
+            string result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+            Console.WriteLine(result);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Task 1: Operators
+            Console.WriteLine("Assignment of Week03");
+            Console.WriteLine("Operator, Contol Flow and Exception Handling");
+
+            //Creating Object of Operators Class
+            Operators ops = new Operators();
+            Console.WriteLine("Operators Task Results");
+            Console.WriteLine("Addition: " + ops.Add(10, 5));
+            Console.WriteLine("Subtraction: " + ops.Subtract(10, 5));
+            Console.WriteLine("Multiplication: " + ops.Multiply(10, 5));
+            Console.WriteLine("Division: " + ops.Divide(10, 5));
+            
+            //Odd Even Finder
+            Console.Write("Odd Even Finder for number 7: ");
+            ops.OddEvenFinder(7);
+            Console.WriteLine();
+        }
+    }
+}
